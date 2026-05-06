54f70dce0ef38f7177e7df46427a2d6f29f29845
rjsanjaymandal <rjsanjaymandal@gmail.com>
2026-05-06 18:45:16 +0530

    feat: implement home page layout with main site components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import PriceTicker from "@/components/PriceTicker";
-import CircleCategories from "@/components/CircleCategories";
-import DiamondShowcase from "@/components/DiamondShowcase";
-import SolitaireShowcase from "@/components/SolitaireShowcase";
-import CollectionsGrid from "@/components/CollectionsGrid";
-import CraftsmanshipSection from "@/components/CraftsmanshipSection";
-import SocialProofSection from "@/components/SocialProofSection";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import PriceTicker from "../components/PriceTicker";
+import CircleCategories from "../components/CircleCategories";
+import DiamondShowcase from "../components/DiamondShowcase";
+import SolitaireShowcase from "../components/SolitaireShowcase";
+import CollectionsGrid from "../components/CollectionsGrid";
+import CraftsmanshipSection from "../components/CraftsmanshipSection";
+import SocialProofSection from "../components/SocialProofSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
